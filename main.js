1596582fd0f1cbdab7b59d463e650cf9c65c4206
JeongHaeun3263 <jhe3263@gmail.com>
2020-06-24 21:11:51 -0400

    js--navbar anchor

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-"use strict";
+'use strict';
 
-const navbar = document.querySelector("#navbar");
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Make navbar transparnet when it is on the top
-document.addEventListener("scroll", () => {
+document.addEventListener('scroll', () => {
   if (window.scrollY > navbarHeight) {
-    navbar.classList.add("navbar--dark");
+    navbar.classList.add('navbar--dark');
   } else {
-    navbar.classList.remove("navbar--dark");
+    navbar.classList.remove('navbar--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
