c96b71df854ba0727025f5d63858a837dbfa7a34
JeongHaeun3263 <jhe3263@gmail.com>
2020-06-26 10:38:32 -0400

    js--transparent

--- main.js
+++ main.js
@@ -30,6 +30,13 @@ contactBtn.addEventListener('click', (event) => {
   scrollIntoView('#contact');
 });
 
+// Make home fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
 // function scroll into view
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
